0921c056d72347f9912a1e4de78aa89e69448172
Krystian K <okamster@o2.pl>
2020-09-20 11:59:21 +0200

    added layout and index

--- src/main/java/sda/finalProject/Budget/controller/RegistrationController.java
+++ src/main/java/sda/finalProject/Budget/controller/RegistrationController.java
@@ -1,15 +1,12 @@
-package sda.finalProject.Budget.Controller;
+package sda.finalProject.Budget.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-import sda.finalProject.Budget.entity.User;
 
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
