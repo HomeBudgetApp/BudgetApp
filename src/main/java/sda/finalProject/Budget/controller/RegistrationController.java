7e829b7f604603d96136254c8d148cc74b5111b9
Krystian K <okamster@o2.pl>
2020-09-20 15:23:07 +0200

    Account repo, entity,service transaction dto exceptions etc.

--- src/main/java/sda/finalProject/Budget/controller/RegistrationController.java
+++ src/main/java/sda/finalProject/Budget/controller/RegistrationController.java
@@ -2,38 +2,58 @@ package sda.finalProject.Budget.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import sda.finalProject.Budget.dto.NewUserDTO;
+import sda.finalProject.Budget.exception.UserLoginExistException;
 import sda.finalProject.Budget.service.UserService;
+import sda.finalProject.Budget.validators.NewUserValidator;
 
 
 @Controller
+@RequestMapping("/registration")
 public class RegistrationController {
-
     private final UserService userService;
+    private final NewUserValidator validator;
 
-    public RegistrationController(UserService userService) {
+
+    public RegistrationController(UserService userService, NewUserValidator validator) {
         this.userService = userService;
+        this.validator = validator;
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET)
-    public ModelAndView showRegister(HttpServletRequest request, HttpServletResponse respone) {
-        ModelAndView mnv = new ModelAndView("register");
-        mnv.addObject("user", new NewUserDTO());
+    @InitBinder
+    void init(WebDataBinder binder) {
+        binder.setValidator(validator);
+    }
 
+    @GetMapping
+    ModelAndView registrationPage() {
+        ModelAndView mnv = new ModelAndView("registration");
+        mnv.addObject("registration", new NewUserDTO());
         return mnv;
     }
 
-    @RequestMapping(value = "/registerProcess", method = RequestMethod.POST)
-    public ModelAndView addUser(HttpServletRequest request, HttpServletResponse respone, @ModelAttribute("user") NewUserDTO user) {
-        userService.createUser(user);
-        return new ModelAndView("welcome", "firstname", user.getFirstName());
+    @PostMapping
+    ModelAndView createNewUser(@ModelAttribute("registration") @Validated NewUserDTO newUserDTO,
+                               BindingResult bindingResult) {
+        if(bindingResult.hasErrors()){
+            return new ModelAndView("registration");
+        }
+        try {
+            userService.createUser(newUserDTO);
+        }catch(UserLoginExistException e){
+            ModelAndView mnv = new ModelAndView("registration");
+            mnv.addObject("error", e.getMessage());
+            return mnv;
+        }
+        return new ModelAndView("redirect:/login");
     }
 }
