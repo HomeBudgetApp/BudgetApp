f143833a245622d95cfd400bf59d8c1d8085bf90
Krystian K <okamster@o2.pl>
2020-09-20 15:37:46 +0200

    layout

--- src/main/java/sda/finalProject/Budget/dto/NewUserDTO.java
+++ src/main/java/sda/finalProject/Budget/dto/NewUserDTO.java
@@ -1,13 +1,11 @@
 package sda.finalProject.Budget.dto;
 
 import lombok.Data;
-import sda.finalProject.Budget.entity.Balance;
 
 import java.math.BigDecimal;
 
 @Data
 public class NewUserDTO {
-
     private String login;
     private String password;
     private String repeatedPassword;
--- src/main/java/sda/finalProject/Budget/entity/UserEntity.java
+++ src/main/java/sda/finalProject/Budget/entity/UserEntity.java
@@ -4,26 +4,26 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.math.BigDecimal;
 
 @Getter
 @Entity
 @NoArgsConstructor
-public class User {
+public class UserEntity {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String username;
-    private BigDecimal balance;
     private String password;
     private String firstName;
     private String lastName;
     private String email;
     private String phone;
 
-    public User(String username, BigDecimal balance, String password, String firstname, String lastname, String email, String phone) {
+    public UserEntity(String username, String password, String firstname, String lastname, String email, String phone) {
         this.username = username;
-        this.balance = balance;
         this.password = password;
         this.firstName = firstname;
         this.lastName = lastname;
